6beeaca94544dd2525e39047ccddd5f50e6e5480
BraytoGBDX <lculpa45@gmail.com>
2025-06-11 11:21:29 -0600

    Add:implementation of swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+   const config = new DocumentBuilder()
+    .setTitle('Mi API')
+    .setDescription('Documentación API con Swagger')
+    .setVersion('1.0')
+    // Puedes agregar auth, tags, etc. aquí
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
