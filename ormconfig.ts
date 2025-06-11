0ddc46d34283cd29c2d2825c4cffceb13d121747
BraytoGBDX <lculpa45@gmail.com>
2025-06-11 10:47:32 -0600

    Add: implementation of env

--- ormconfig.ts
+++ ormconfig.ts
@@ -5,16 +5,19 @@ import { Entity } from './src/entities/entity.entity';
 import { Sensor } from './src/sensors/sensor.entity';
 import { SensorReading } from './src/sensor-readings/sensor-reading.entity';
 import { Bill } from './src/bills/bill.entity';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const config: DataSourceOptions = {
   type: 'mysql',
-  host: 'localhost',
-  port: 3306,
-  username: 'root',           // Ajusta según tu usuario MySQL
-  password: '1234',  // Ajusta según tu password
-  database: 'aqua_db', // Crea esta BD en tu MySQL
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT): undefined,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   entities: [User, Entity, Sensor, SensorReading, Bill],
-  synchronize: true,          // En desarrollo: sincroniza entidades con la BD
+  synchronize: true,
   logging: false,
 };
 
