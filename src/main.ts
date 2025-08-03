210f2e2a0cfa680c86a9629cf062d947f39bb7c5
BraytoGBDX <lculpa45@gmail.com>
2025-08-03 17:20:54 -0600

    Add: cors policy

--- src/main.ts
+++ src/main.ts
@@ -34,6 +34,11 @@ async function bootstrap() {
     console.log('✅ Usuario admin creado: admin@aqua.com / admin1234');
   }
 
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
