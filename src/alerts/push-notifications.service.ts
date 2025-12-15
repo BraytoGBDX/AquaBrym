527644abfe793f62b6d68c2e42bd1ab7903309e2
BraytoGBDX <lculpa45@gmail.com>
2025-12-15 15:19:05 -0600

    Fix: push notifications imports

--- src/alerts/push-notifications.service.ts
+++ src/alerts/push-notifications.service.ts
@@ -17,8 +17,11 @@ export class PushNotificationsService {
     try {
       const response = await webPush.sendNotification(subscription, JSON.stringify(payload));
       console.log('Notificación enviada:', response);
+      console.log('Push OK:', response.statusCode);
     } catch (error) {
       console.error('Error al enviar la notificación', error);
+      console.error('Push ERROR:', error.statusCode, error.body);
+
     }
   }
 
@@ -26,10 +29,10 @@ export class PushNotificationsService {
   async sendTestPushNotification() {
     // Suscripción hardcodeada para pruebas
     const subscription = {
-      endpoint: "https://wns2-bn3p.notify.windows.com/w/?token=BQYAAACNya6n1v5jI5Bzv2%2ft4TqleCbsahV7evi53USPLEMZbKUtF7O11Zx86al06LxISb5O1d2H3c4ePtOpeBPToHhlmol5aLAURwNL6Hvlc3qAsG5TxFr2yII1HKlxUbU9eVKaVYph%2fQ7JkeeR7wDWiYIt5VUQPMWEVFYVvGS7E4rFQ7f%2bYhrAmDaoTo1hiCwaj6wc9Hiz7d7O9R7f2Z7HdzD9aWDBD1%2fEZpX5TJppxV5rhJkaLO46Ehs3JEeksW6RtWmQ4W3Kzx0s0jJmZkcfGf5B1cnV%2f0Gy9Sj9rXmNpF99zdY73idev2xeiULP3hbOx%2b%2fL3uUgQW4m0Hu4IHZIV%2fftVIyYvoxTkldIvsxAY3KtQA%3d%3d",
-      keys: { 
-        auth: 'l3tauK-_tV41bMWa3lMKZw',
-        p256dh: 'BE1BOutsVd9qPcn-d2PfqcytI-vxwI0_MGxy88TdsjNQULXiORNuiPt_bu7aR-cse6yKXzxkTHf1T_vt6AnFTBw'
+      endpoint: "https://wns2-bn3p.notify.windows.com/w/?token=BQYAAABdO%2f6PdWss9jQ7IhaGWZc0FMuQd7oh3uKtzrlxCxbd6MLPU2ZU%2bGe5TD7XkAZIQgNGRxp46%2bM%2fxEcskLbBrPCdBNaBU%2fEDjLUA29ZAHbjBjaflWB%2fuWCzLyudp0eAiId1s2wOf4H3Mljvm8fngDlw0lGGOCT2UjZAW1pOHYV%2bip6r8bUCimgnzLXDQMDNfvInOBSkt5H5gRddeBYedlBlUa8F6E%2fjpVeroW7D9zCc%2beZHnXEs5vyT0Df%2f3wDUwNCfOMNGyQZKVc%2f2K3UDFRW9ik36zwZON6XAb5nlFgD6np0BydSAubTcqNnj3912k91Ccz2f%2b2QocUu6bg1wjOJvFlImemKA4JD4qXrlJEJwzsg%3d%3d",
+        keys: { 
+        auth: 'nLjnXnVvEk0sY6-Efn8sHA',
+        p256dh: 'BEp-zLbx3ACZvw0dH8wkRntVdwvWnL9H8prBwnLY12m6mxg6VX4Lw67P3uqoxVVRN3YimoO7F6Zku7RmJpOkqq0'
       }
     };
 
